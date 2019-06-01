f60ccf72e3f18cf5ba8f6ffb76f9a9ad1f4cb710
Devin Powell <dpowel11@kent.edu>
2019-05-31 19:52:11 -0600

    Added Main Info Section - Basic

---
+++ assets/javascript/java.js
@@ -0,0 +1,13 @@
+// ================
+// VARIABLES
+// ================
+
+
+// ================
+// FUNCTIONS
+// ================
+$('#forums').on('click', function() {
+    event.preventDefault();
+    // Redirect to Forums Page
+    console.log('forums button clicked');
+});
